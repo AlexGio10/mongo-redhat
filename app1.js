9a0e0623c22f5a769e4362771b05c640989732ff
AlexGio10 <150153930+AlexGio10@users.noreply.github.com>
2024-08-22 09:50:23 -0600

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Conectar a MongoDB Atlas usando el enlace proporcionado
+mongoose.connect('mongodb+srv://andre2:1234@myatlasclusteredu.zamwt2c.mongodb.net/?retryWrites=true&w=majority&appName=myAtlasClusterEDU', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Conectado a MongoDB Atlas');
+}).catch(err => {
+    console.error('Error al conectar a MongoDB', err);
+});
+
+app.get('/', (req, res) => {
+    res.send('¡Aplicación funcionando!');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${port}`);
+});
