8e76dc7c2979bfec987454eae8722f71eb47ca9b
AlexGio10 <150153930+AlexGio10@users.noreply.github.com>
2024-08-23 17:00:40 -0600

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+ 
 const app = express();
 const port = process.env.PORT || 8080;
-
+ 
+let appStatus = 'Aplicación en proceso de inicialización';
+ 
 // Conectar a MongoDB Atlas usando el enlace proporcionado
 mongoose.connect('mongodb+srv://andre2:1234@myatlasclusteredu.zamwt2c.mongodb.net/?retryWrites=true&w=majority&appName=myAtlasClusterEDU', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
     console.log('Conectado a MongoDB Atlas');
+    appStatus = 'Aplicación funcionando';
 }).catch(err => {
     console.error('Error al conectar a MongoDB', err);
+    appStatus = 'Aplicación no funcionando';
 });
-
+ 
 app.get('/', (req, res) => {
-    res.send('¡Aplicación funcionando!');
+    res.send(appStatus);
 });
-
+ 
 app.listen(port, () => {
     console.log(`Servidor ejecutándose en http://localhost:${port}`);
 });
