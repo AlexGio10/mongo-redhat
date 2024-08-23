9db3ad77abe26968565ababb5f39a0ae8099ccb2
AlexGio10 <150153930+AlexGio10@users.noreply.github.com>
2024-08-23 16:12:07 -0600

    Update app1.js

--- app1.js
+++ app1.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // Conectar a MongoDB Atlas usando el enlace proporcionado
-mongoose.connect('mongodb+srv://andre2:1234@myatlasclusteredu.zamwt2c.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=myAtlasClusterEDU', {
+mongoose.connect('mongodb+srv://andre2:1234@myatlasclusteredu.zamwt2c.mongodb.net/?retryWrites=true&w=majority&appName=myAtlasClusterEDU', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
@@ -14,35 +14,10 @@ mongoose.connect('mongodb+srv://andre2:1234@myatlasclusteredu.zamwt2c.mongodb.ne
     console.error('Error al conectar a MongoDB', err);
 });
 
-// Definir el esquema y el modelo
-const movieSchema = new mongoose.Schema({
-    title: String,
-    director: String,
-    year: Number,
-    // Otros campos que pueda tener el documento en la colección
+app.get('/', (req, res) => {
+    res.send('¡Aplicación funcionando!');
 });
 
-const Movie = mongoose.model('Movie', movieSchema, 'movies');
-
-// Ruta principal que realiza la consulta a MongoDB y muestra el resultado
-app.get('/', async (req, res) => {
-    try {
-        // Ejemplo de consulta: encontrar todas las películas dirigidas por un director específico
-        const director = req.query.director || 'Steven Spielberg'; // Ejemplo de parámetro de consulta
-        const movies = await Movie.find({ director: director });
-
-        if (movies.length > 0) {
-            res.send(`Películas dirigidas por ${director}:<br>${movies.map(movie => `- ${movie.title} (${movie.year})`).join('<br>')}`);
-        } else {
-            res.send(`No se encontraron películas dirigidas por ${director}.`);
-        }
-    } catch (err) {
-        console.error('Error al realizar la consulta', err);
-        res.status(500).send('Error interno del servidor');
-    }
-});
-
-// Iniciar el servidor
 app.listen(port, () => {
-    console.log(`http://ejemploredhat-mongodb-demo.apps.8262f.dynamic.redhatworkshops.io/:${port}`);
+    console.log(`Servidor ejecutándose en http://localhost:${port}`);
 });
